fa7898e5740dda647931a8021254dd6dddf42384
theUnixe <rahulbanik0001@gmail.com>
2022-07-10 21:40:24 -0400

    firebase_new_connection

--- index.js
+++ index.js
@@ -11,13 +11,14 @@ const path =  require('path');
 
 
 
-let  serviceAccount = require("./easyrent-aa461-firebase-adminsdk-gb1wy-0772e0d20a.json");
-const { database } = require('firebase-admin');
+// let  serviceAccount = require("./easyrent-aa461-firebase-adminsdk-gb1wy-0772e0d20a.json");
+// const { database } = require('firebase-admin');
+// const { static } = require('express');
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-let db= admin.firestore();
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount)
+// });
+// let db= admin.firestore();
 
 let staticPath =path.join(__dirname,);
 
@@ -32,54 +33,82 @@ app.get ('/', (req, res) => {
     res.sendFile(path.join(staticPath,"index.html"));
 
 })
-app.get('/signup', (req,res)=>{
-    res.sendFile(path.join(staticPath,'signup.html'));
+app.get('/login', (res, req)=>{
+    res.sendFile(path.join(staticPath, "login.html"))
 })
+// app.post('/login',(req,res)=>{
+//     let {email, password } =req.body;
 
-app.post('/signup',(req, res) =>{
-    console.log(req.body)
-    res.json("data recived")
-    let{name ,email ,password , number, tac} = req.body;
+//     if(!email.length || !password.length){
+//         return res.json({'alert':'fill all the inputs'})
+//     }
+//     db.collection('users').doc(email).get()
+//     .then(user=>{
+//         if(!user.exists){
+//             return res.json({'alert':'log in email does not exists'})
+//         }else{
+//             bcrypt.compare(password,user.data().password,(err,result)=>{
+//                 if(ressult){
+//                     return res.json({
+//                        name: data.name,
+//                        email:data.email,
+//                        seller:data.seller, 
+//                     })
+//                 }else{
+//                     return res.json({'alert':'password in incorrect'});
+//                 }
+//             })
+//         }
+//     })
+// })
+// app.get('/signup', (req,res)=>{
+//     res.sendFile(path.join(staticPath,'signup.html'));
+// })
+
+// app.post('/signup',(req, res) =>{
+//     console.log(req.body)
+//     res.json("data recived")
+//     let{name ,email ,password , number, tac} = req.body;
     
-    //form validation
-    if(name.length < 3){
-        return res.json({'alert': 'name must be 3 letters long'})
-    } else if(!email.length ){
-        return res.json({'alert':'Email is required'});
-    } else if(password.length < 8){
-        return res.json({'alert':'Password must be at least 8 characters long'});
-    }  else if(!number.length){
-        return res.json({'alert':'Number is required'});
+//     //form validation
+//     if(name.length < 3){
+//         return res.json({'alert': 'name must be 3 letters long'})
+//     } else if(!email.length ){
+//         return res.json({'alert':'Email is required'});
+//     } else if(password.length < 8){
+//         return res.json({'alert':'Password must be at least 8 characters long'});
+//     }  else if(!number.length){
+//         return res.json({'alert':'Number is required'});
 
-    } else if(!Number(number) || number.length < 10){
-        return res.json({'alert':'Number must be at least 10 digits long'});
-    } else if(!tac){
-        return res.json({'alert':'You must agree to our terms and conditions'});
-    } 
-    //store user in db
-    db.collection('users').doc(email).get()
-    .then(user => {
-        if(user.exists){
-            return res.json({'alert':'User already exists'});
-        } else{
-// now we will encrypt before storing it.
-            bcrypt.genSalt(10, (err, salt) => {
-                bcrypt.hash(password, salt, (err, hash) => {
-                    req.body.password = hash;
-                    db.collection('users').doc(email).set(req.body)
-                    .then(data => {
-                        res.json({
-                            name: req.body.name,
-                            email: req.body.email,
-                            // seller: req.body.seller,
-                        })
-                    })
-                })
-            })
+//     } else if(!Number(number) || number.length < 10){
+//         return res.json({'alert':'Number must be at least 10 digits long'});
+//     } else if(!tac){
+//         return res.json({'alert':'You must agree to our terms and conditions'});
+//     } 
+//     //store user in db
+//     db.collection('users').doc(email).get()
+//     .then(user => {
+//         if(user.exists){
+//             return res.json({'alert':'User already exists'});
+//         } else{
+// // now we will encrypt before storing it.
+//             bcrypt.genSalt(10, (err, salt) => {
+//                 bcrypt.hash(password, salt, (err, hash) => {
+//                     req.body.password = hash;
+//                     db.collection('users').doc(email).set(req.body)
+//                     .then(data => {
+//                         res.json({
+//                             name: req.body.name,
+//                             email: req.body.email,
+//                             // seller: req.body.seller,
+//                         })
+//                     })
+//                 })
+//             })
 
-        }
-    })
-})
+//         }
+//     })
+// })
 
 app.listen(4000,()=>{
     console.log('listening on port 40000')
